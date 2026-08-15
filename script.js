831d9748f44c019790784389b77cba96018f0ad8
Saquib Anjum <134352662+Saquib-Anjum@users.noreply.github.com>
2026-08-15 18:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const level = document.getElementById("level");
+let count =0;
+while(level 1=null){
+	count+=1;
+	level=level.parentElement;
+}
+alert(`The level of the element is: ${count}`)
